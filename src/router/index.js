4170c5c1dae937e61a382703d3c451b7d1073ca1
Mj-2514 <manjeet.s@inte-qt.com>
2025-10-17 18:54:46 +0530

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,3 @@
-// router/index.js
 import { createRouter, createWebHistory } from 'vue-router'
 import RecipeView from '../views/RecipeView.vue'
 import About from '@/views/About.vue'
@@ -6,27 +5,15 @@ import Contact from '@/views/Contact.vue'
 import HeroSection from '@/components/HeroSection.vue'
 import SearchResults from '@/views/SearchResults.vue'
 
-const routes = [
-  { path: '/', name: 'Home', component: HeroSection },
-  { path: '/recipe/:id', name: 'Recipe', component: RecipeView },
-  { path: '/about', name: 'About', component: About },
-  { path: '/contact', name: 'Contact', component: Contact },
-  { path: '/search', name: 'SearchResults', component: SearchResults }
-]
-
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about', 
-    component: AboutView
-  }
-]
+    { path: '/', name: 'Home', component: HeroSection },
+    { path: '/recipe/:id', name: 'Recipe', component: RecipeView },
+    { path: '/about', name: 'About', component: About },
+    { path: '/contact', name: 'Contact', component: Contact },
+    { path: '/search', name: 'SearchResults', component: SearchResults }
+  ]
 })
 
 // Make sure this is a default export
